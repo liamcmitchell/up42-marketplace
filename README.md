da9f6095526a2fedc8de89c5e19543ec0876585e
Liam Mitchell <git@liam.geek.nz>
2022-05-10 21:50:40 +0200

    Clean, install prettier, tried setting TS up, compiler issue, reverted

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # UP42 Marketplace
 
 - Starting from [netlify template](https://github.com/netlify-templates/next-netlify-starter)
+- Clean, install prettier, tried setting TS up, compiler issue, reverted
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>UP42 Marketplace</title>
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+      <div>
+        <nav>
+          <h1>UP42 Marketplace</h1>
+        </nav>
+      </div>
+    </>
+  );
 }
