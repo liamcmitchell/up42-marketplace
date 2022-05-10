f092c2332c392314141275978fb2bd8a086eaff3
Liam Mitchell <git@liam.geek.nz>
2022-05-10 22:21:25 +0200

    Layout

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Starting from [netlify template](https://github.com/netlify-templates/next-netlify-starter)
 - Clean, install prettier, tried setting TS up, compiler issue, reverted
+- Layout, using inline styles for simplicity, not planning on doing mobile layout
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,46 @@ export default function Home() {
         <title>UP42 Marketplace</title>
       </Head>
 
-      <div>
-        <nav>
-          <h1>UP42 Marketplace</h1>
-        </nav>
-      </div>
+      <nav
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 80,
+          borderBottom: "1px solid #eee",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 16,
+        }}
+      >
+        <h1>UP42 Marketplace</h1>
+        <div>Credits: 10000</div>
+      </nav>
+      <main
+        style={{
+          paddingTop: 80,
+          display: "flex",
+        }}
+      >
+        <div style={{ margin: 16 }}>Loading...</div>
+      </main>
+      <aside
+        style={{
+          position: "fixed",
+          top: 80,
+          right: 0,
+          bottom: 0,
+          width: 320,
+          borderLeft: "1px solid #ccc",
+          display: "flex",
+          flexDirection: "column",
+          overflowY: "auto",
+        }}
+      >
+        <h2 style={{ margin: 16 }}>Cart</h2>
+      </aside>
     </>
   );
 }
